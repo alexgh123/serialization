96ea80315cba6359f5ae920d79cda6533f26000d
Alex Hardt <hardt.alex@gmail.com>
2015-02-13 13:05:25 -0600

    starting to add new director for compiled letters to go

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -20,6 +20,7 @@ template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
@@ -28,6 +29,10 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  puts form_letter
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 
 end
