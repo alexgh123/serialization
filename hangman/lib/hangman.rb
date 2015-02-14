3d58e67e64dd67a9e3856ea72f47e7ed45805640
Alex Hardt <hardt.alex@gmail.com>
2015-02-14 10:54:05 -0600

    word picker working, between 5 and 12, clean string

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -7,8 +7,25 @@
 #there is prob a way to load the list everytime, but i'm not sure its necessary
 
 
+def new_game
+  #load dictionary, pick a random word that is 5-12 characters long
+  dictionary = File.readlines("../words.txt")
+  word = dictionary.sample
+  word = word[0..-2] #remove new line character
+  if word.length < 12 && word.length > 5
+    word
+  else
+    word = dictionary.sample
+    word = word[0..-2]
+  end
+  word
 
+  # until word.length.betweeen?(5,12) do
+  #   word = dictionary.sample
+  # end
+  # p word
+  #don't forget to strip new line chars
+end
 
-dictionary = File.readlines("../words.txt")
+p new_game
 
-dictionary
